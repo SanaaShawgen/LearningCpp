93a22c0a6828ffc850870887c43350bb4360106f
SanaaShawgen <sana07sh@gmaail.com>
2021-08-10 22:04:06 +0300

    add enum practice

---
+++ enumquiz.cpp
@@ -0,0 +1,13 @@
+
+enum Monster
+{
+    monster_orcs,
+    monster_goblines,
+    monster_trolls,
+    monster_ogres,
+    monster_skeletons
+};
+int main()
+{
+    Monster m{Monster::monster_trolls};
+}
